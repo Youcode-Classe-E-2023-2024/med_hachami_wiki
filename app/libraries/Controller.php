fc419eeee36231b9648ba1ff6966cd635d3f3453
MedHachami <moha.hach.9@gmail.com>
2024-01-07 22:54:10 +0100

    Update Controller.php

--- app/libraries/Controller.php
+++ app/libraries/Controller.php
@@ -14,15 +14,15 @@
     }
 
     // Load view
-    // public function view($view, $data = []){
-    //   // Check for view file
-    //   if(file_exists('../app/views/' . $view . '.php')){
-    //     require_once '../app/views/' . $view . '.php';
-    //   } else {
-    //     // View does not exist
-    //     // die('View does not exist');
-    //     echo "View does not exist";
-    //   }
-    // }
+    public function view($view, $data = []){
+      // Check for view file
+      if(file_exists('../app/views/' . $view . '.php')){
+        require_once '../app/views/' . $view . '.php';
+      } else {
+        // View does not exist
+        // die('View does not exist');
+        echo "View does not exist";
+      }
+    }
   }
   ?>
