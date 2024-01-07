14fecebe33854ae44f1bf31c16aa805329018e00
MedHachami <moha.hach.9@gmail.com>
2024-01-07 23:46:48 +0100

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,6 +2,9 @@
   // Load Config
   require_once 'config/config.php';
 
+  // Load Helpers
+  require_once 'helpers/url_helper.php';
+  require_once 'helpers/session_helper.php';
   
   require_once 'middleware/AuthMiddleware.php';
   require_once 'models/JwtAuth.php';
