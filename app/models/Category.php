5486e2c90284c87f3498365104cef408220e4b78
MedHachami <moha.hach.9@gmail.com>
2024-01-08 12:24:30 +0100

    Create Category.php

---
+++ app/models/Category.php
@@ -0,0 +1,40 @@
+<?php
+
+Class Category{
+    private $db;
+    public function __construct(){
+        $this->db = new Database();
+    }
+
+    public function allCategories(){
+        $this->db->query("SELECT * FROM category");
+        return $this->db->resultSet();
+    }
+    public function addNewCategory($data){
+        $this->db->query("INSERT INTO category(name,description	) VALUES (:category_name , :category_description )");
+        $this->db->bind(":category_name",$data['name']);
+        $this->db->bind(":category_description",$data['description']);
+
+        return $this->db->execute();
+    }
+
+    public function getCategoryById($id){
+        $this->db->query("SELECT * FROM category where id = :id");
+        $this->db->bind(":id" , $id);
+        return $this->db->single();
+    }
+
+    public function editCategory($data){
+        $this->db->query("UPDATE category SET name = :name, description = :description WHERE id = :id");
+        $this->db->bind(":name" , $data["name"]);
+        $this->db->bind(":description" , $data["description"]);
+        $this->db->bind(":id" , $data["id"]);
+        return $this->db->execute();
+    }
+
+    public function deleteCategory($data){
+        $this->db->query("DELETE FROM category WHERE id = :id");
+        $this->db->bind(":id" , $data["id"]);
+        return $this->db->execute();
+    }
+}
